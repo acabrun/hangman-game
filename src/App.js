1ddd30249cf9c730780e399a98baf4be4be924f1
acabrun <acabrun@gmail.com>
2019-12-05 15:35:05 +1100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+const phrases = ["ORDINATEUR", "BUREAU", "CONFITURE", "LIVRE", "DEVELOPPEUR"];
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      usedLetters: "",
+      guessingWord: ""
+    };
+  }
+
+  // Mask
+  computeDisplay(phrase, usedLetters) {
+    return phrase.replace(/\w/g, letter =>
+      usedLetters.includes(letter) ? letter : "_"
+    );
+    console.log(this.computeDisplay)
+  }
+
+  getRandomPhrase() {
+    var newWord = phrases[Math.floor(Math.random() * Math.floor(phrases.length))]
+    this.setState({guessingWord: newWord})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {console.log(
+          `Mot à deviner : ${this.state.guessingWord}, Lettres comparées : ${this.state.usedLetters}`
+        )}
+        <header className="App-header">{this.state.guessingWord}</header>
+        <body>
+          <div className="alphabet">
+            {letters.map((letter) => (
+              <button
+                className="alphabet-button"
+                key={letter}
+                onClick={() => this.computeDisplay(this.state.guessingWord, letter)}
+              >
+                {letter}
+              </button>
+            ))}
+          </div>
+          <div>
+            <button className="newgame-button" onClick={() => {this.getRandomPhrase()}} >New Game</button>
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
-
-export default App;
